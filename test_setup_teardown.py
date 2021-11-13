e1a0472d05ecb5d3f0ef401b40f9f339c641f67a
Hasan Özkök <hasan.ozkok@akinon.com>
2021-11-13 22:39:42 +0300

    setup and teardown concepts

---
+++ test_setup_teardown.py
@@ -0,0 +1,35 @@
+
+def setup_module(module):
+    print("\nSetup Module")
+
+
+def teardown_module(module):
+    print("\nTeardown Module")
+
+
+def setup_function(function):
+    if function == test1:
+        print("\nSetting up test1!")
+    elif function == test2:
+        print("\nSetting up test2!")
+    else:
+        print("\nSetting up unknown test!")
+
+
+def teardown_function(function):
+    if function == test1:
+        print("\nTearing down test1!")
+    elif function == test2:
+        print("\nTearing down test2!")
+    else:
+        print("\nTearing down unknown test!")
+
+
+def test1():
+    print("Executing test1!")
+    assert True
+
+
+def test2():
+    print("Executing test2!")
+    assert True
